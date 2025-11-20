3535ee614cce92b9ad7c264cac6c0c07f4e917cf
Gourav Gupta <igouravg@gmail.com>
2025-11-20 23:58:06 +0530

    implement auth logic

---
+++ lib/nodemailer/index.ts
@@ -0,0 +1,36 @@
+import nodemailer from "nodemailer";
+import { WELCOME_EMAIL_TEMPLATE } from "./templates";
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.NODEMAILER_EMAIL!,
+    pass: process.env.NODEMAILER_PASSWORD || process.env.NOEMAILER_PASSWORD,
+  },
+});
+
+export const sendWelcomeEmail = async ({
+  email,
+  name,
+  intro,
+}: WelcomeEmailData) => {
+  const htmlTemplate = WELCOME_EMAIL_TEMPLATE.replace("{{name}}", name).replace(
+    "{{intro}}",
+    intro
+  );
+
+  const mailOptions = {
+    from: `"Signalist" <signalist@promail.com>`,
+    to: email,
+    subject: `Welcome to Signalist - your stock market toolkit is ready!`,
+    text: "Thanks for joining Signalist",
+    html: htmlTemplate,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (err) {
+    console.error("Failed to send welcome email:", err);
+    throw err;
+  }
+};
